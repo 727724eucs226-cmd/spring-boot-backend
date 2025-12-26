a7f050b8a79c41aea8b145e07928f10b74bc9087
727724eucs226-cmd <727724eucs226@skcet.ac.in>
2025-12-26 18:22:47 +0530

    Update Guarantor.java

---
+++ main/java/com/examly/springapp/model/Guarantor.java
@@ -0,0 +1,19 @@
+package com.examly.springapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Guarantor {
+    @Id
+    private Long id;
+    private String name;
+
+    public Guarantor() {}
+    public Guarantor(Long id, String name) { this.id = id; this.name = name; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
